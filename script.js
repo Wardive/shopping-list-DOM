9eab82e663584e9288e99350c8322c538afad1f5
Cristian Dinu <ezelzar12@gmail.com>
2019-11-10 20:02:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var btn = document.querySelector("ul");
-
+var btnDel = document.querySelector("ul");
 
 function inputLength() {
 	return input.value.length;
@@ -14,18 +14,23 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = "";
 
-	// Creates a Button next to the li element each time a li element is created
+	// Creates a Button that toogles a css class(it this case it lines-through)
 	var btn = document.createElement("button");
 	btn.appendChild(document.createTextNode("Done!"));
 	li.appendChild(btn);
 
+	// Creates a Button that deletes the li element
+	var btnDel = document.createElement("button");
+	btnDel.appendChild(document.createTextNode("Delete"));
+	li.appendChild(btnDel);
+
 	// after the button is clicked the deleteElement function will be called and remove the parent element of the button in this case the li.
-	btn.addEventListener('click', deleteElement);
+	btnDel.addEventListener('click', deleteElement);
 
 	// after li element is clicked the toggleClass function will be called
-	li.addEventListener('click', toggleClass);
+	btn.addEventListener('click', toggleClass);
 
-	// toogle the class(done) of the li element
+	// toogles the class(done) of the li element
 	function toggleClass() {
 		li.classList.toggle("done");
 	}
